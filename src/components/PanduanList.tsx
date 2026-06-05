8fcdee79704c5c7c01fb68a125a81d13b1d60598
it-planetekosistek <it-planetekosistek@gmail.com>
2026-06-05 13:06:40 +0700

    tambah /panduan

--- src/components/PanduanList.tsx
+++ src/components/PanduanList.tsx
@@ -97,7 +97,7 @@ export default function PanduanList() {
         Panduan Planet Pulsa
       </h1>
       <p className="text-gray-600 mb-10">
-        Panduan menggunakan aplikasi Planet Pulsa.
+        Panduan menggunakan aplikasi Planet Pulsa. Panduan yang belum tersedia bisa menghubungi kami melalui halaman kontak.
       </p>
 
       {sortedPanduanList.length === 0 ? (
